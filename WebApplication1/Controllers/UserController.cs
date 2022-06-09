ea04de4f27524bde8d1992b1e8845eadb28685eb
JamesIsler <isl1122@calu.edu>
2022-06-09 12:50:19 -0400

    Add project files.

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var userModel = new UserModel();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel user)
+        {
+            int userId = user.Id;
+            string firstName = user.FirstName;
+            string lastName = user.LastName;
+            byte age = user.Age;
+            DateTime registeredDate = user.RegisterDate;
+            ViewData["Id"] = userId;
+            ViewData["FirstName"] = firstName;
+            ViewData["LastName"] = lastName;
+            ViewData["Age"] = age;
+            ViewData["RegisteredDate"] = registeredDate;
+
+
+            return View() ;
+        }
+
+        public string TestMethod(string firstName, string lastName)
+        {
+            string testMessage = $"This is the TestMethod return value, FirstName: {firstName}, LastName: {lastName}";
+            ViewData["TestMessage"] = testMessage;
+            return testMessage; 
+        }
+
+
+        
+    }
+}
---
+++ WebApplication1/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
